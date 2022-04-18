785caac4f5d9a1bc3659c25ece86b2848a0d63aa
Navneet Kumar <navneetharsh3@gmail.com>
2022-04-19 01:13:21 +0530

    a

---
+++ src/Calender.jsx
@@ -0,0 +1,26 @@
+
+const MonthNameShort=[
+    'Jan',
+    'Feb',
+    'Mar',
+    'Apr',
+    'May',
+    'Jun',
+    'Jul',
+    'Aug',
+    'Sep',
+    'Oct',
+    'Nov',
+    'Dec'
+  ]
+  const weekdayNameShort = ['Sun', 'Mon', 'Tue', 'Wed','Thu','Fri','Sat'];
+  
+  
+export const Calender = ()=>{
+
+    return(
+        <div>
+
+        </div>
+    )
+}
